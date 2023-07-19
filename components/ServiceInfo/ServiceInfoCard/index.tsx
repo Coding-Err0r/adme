3304418f37171bc97694e55c508d6f267cc12944
Rhine <rhine.cse@gmail.com>
2023-07-19 12:30:07 +0600

    ⚒️ fix : service card fixed

--- components/ServiceInfo/ServiceInfoCard/index.tsx
+++ components/ServiceInfo/ServiceInfoCard/index.tsx
@@ -8,15 +8,15 @@ interface Props {
 
 const ServiceInfoCard = ({ title, image }: Props) => {
   return (
-    <div className="flex flex-col items-center px-8 bg-white shadow-lg rounded-bl-[40px] rounded-xl py-6">
+    <div className="flex flex-col items-center p-6 bg-white shadow-lg rounded-bl-[40px] rounded-xl  space-y-4">
       <Image
         src={image}
         alt=""
         width={2000}
         height={2000}
-        className="h-14 w-14"
+        className="object-contain h-14 w-14"
       />
-      <p className="text-lg font-semibold text-black">{title}</p>
+      <p className="text-base font-semibold text-black">{title}</p>
     </div>
   );
 };
