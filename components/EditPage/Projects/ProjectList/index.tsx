8c98e93529662ee9f91d8e6ee9e5c547123ce6d0
Rhine <rhine.cse@gmail.com>
2023-08-02 11:40:07 +0600

    ⚒️ Fix : Working on sign up page

--- components/EditPage/Projects/ProjectList/index.tsx
+++ components/EditPage/Projects/ProjectList/index.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React, { useEffect, useState } from "react";
-import parse from "html-react-parser";
 
 import FolderAtIcon from "../../SVGIcons/FolderAtIcon";
 interface Props {
