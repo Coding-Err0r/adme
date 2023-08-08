451552a18e816a5c34ed1c081aff885f9ab21b9c
Rhine <rhine.cse@gmail.com>
2023-08-08 16:11:28 +0600

    ⚒️ Fix : Working on sign up page

--- components/CustomerService/Chatbox/ReceiverText/index.tsx
+++ components/CustomerService/Chatbox/ReceiverText/index.tsx
@@ -9,7 +9,7 @@ const ReceiverText = ({ text }: Props) => {
       <div className="flex items-end justify-end">
         <div className="flex flex-col items-end order-1 max-w-xs mx-2 space-y-2 text-xs">
           <div>
-            <span className="inline-block px-6 py-3 text-sm text-black bg-gray-200 rounded-full rounded-br-none md:text-base md:py-4">
+            <span className="inline-block px-6 py-3 text-sm text-black rounded-full rounded-br-none bg-gray-2 00 md:text-base md:py-4">
               {text}
             </span>
           </div>
